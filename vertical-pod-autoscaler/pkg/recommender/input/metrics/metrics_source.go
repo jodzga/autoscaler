664fc31c1fc3f69515e3afc816b8d21e467ef1ba
Kathy Lee <kathy.lee@databricks.com>
2024-04-10 00:01:10 -0700

    use pod naemspace

--- vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_source.go
+++ vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_source.go
@@ -61,7 +61,7 @@ func (s podMetricsSource) List(ctx context.Context, namespace string, opts v1.Li
 
 	for _, pod := range podMetrics.Items {
 		for _, container := range pod.Containers {
-			baseURL := fmt.Sprintf("http://m3coord-read-regional-internal-svc.m3.svc.cluster.local:7201/api/v1/query?query='max_over_time(container_memory_rss{container_name=%s,pod_name=%s,namespace=%s}[5m])'", container.Name, pod.Name, namespace)
+			baseURL := fmt.Sprintf("http://m3coord-read-regional-internal-svc.m3.svc.cluster.local:7201/api/v1/query?query='max_over_time(container_memory_rss{container_name=%s,pod_name=%s,namespace=%s}[5m])'", container.Name, pod.Name, pod.Namespace)
 			// parsedURL, err := url.Parse(baseURL)
 			// if err != nil {
 			// 	log.Fatalf("Error parsing base URL: %v", err)
