34b9bcf4d05aea70ef916e3d09974db17177a95f
Kathy Lee <kathy.lee@databricks.com>
2024-04-10 12:45:20 -0700

    pushing cleaned up version

--- vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_source.go
+++ vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_source.go
@@ -17,13 +17,12 @@ limitations under the License.
 package metrics
 
 import (
+	"context"
 	"encoding/json"
-    "io/ioutil"
-    "log"
 	"fmt"
+    "io/ioutil"
 	"net/url"
     "net/http"
-	"context"
 	k8sapiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -37,6 +36,9 @@ import (
 	"k8s.io/metrics/pkg/client/external_metrics"
 	"time"
 )
+
+const M3Url = "http://m3coord-read-regional-internal-svc.m3.svc.cluster.local:7201"
+const ResourceRSS k8sapiv1.ResourceName = "rss"
 
 // PodMetricsLister wraps both metrics-client and External Metrics
 type PodMetricsLister interface {
@@ -57,64 +59,80 @@ func (s podMetricsSource) List(ctx context.Context, namespace string, opts v1.Li
 	podMetricsInterface := s.metricsGetter.PodMetricses(namespace)
 	podMetrics, err := podMetricsInterface.List(ctx, opts)
 	if err != nil {
-		klog.Errorf("Error getting pod metrics: %v", err)
+		klog.ErrorS(err, "Error while listing pod metrics")
 		return nil, err
 	}
 
-	for _, pod := range podMetrics.Items {
-		for _, container := range pod.Containers {
-			baseURL, err := url.Parse("http://m3coord-read-regional-internal-svc.m3.svc.cluster.local:7201")
+	baseUrl, err := url.Parse(M3Url)
+	if err != nil {
+		klog.ErrorS(err, "Error while parsing M3 URL")
+		return nil, err
+	}
+	baseUrl.Path += "/api/v1/query"
+
+	for i, pod := range podMetrics.Items {
+		for j, container := range pod.Containers {
+			parameters := url.Values{}
+			query := fmt.Sprintf("max_over_time(container_memory_rss{container_name='%s', pod_name='%s', namespace='%s'}[5m])", container.Name, pod.Name, pod.Namespace)
+			parameters.Add("query", query)
+			baseUrl.RawQuery = parameters.Encode()
+			
+			resp, err := http.Get(baseUrl.String())
 			if err != nil {
-				log.Fatalf("Error parsing base URL: %v", err)
+				klog.ErrorS(err, "Error while querying M3", "query", query)
+				continue
 			}
 
-			baseURL.Path += "/api/v1/query"
-			parameters := url.Values{}
-			parameters.Add("query", fmt.Sprintf("max_over_time(container_memory_rss{container_name='%s', pod_name='%s', namespace='%s'}[5m])", container.Name, pod.Name, pod.Namespace))
-			baseURL.RawQuery = parameters.Encode()
-			
-			print(baseURL.String())
-			resp, err := http.Get(baseURL.String())
-			if err != nil {
-				log.Fatalf("Error occurred making reaquest: %v", err)
-			}
 			defer resp.Body.Close()
 			body, err := ioutil.ReadAll(resp.Body)
 			if err != nil {
-				log.Fatalf("Error reading response body: %v", err)
+				klog.ErrorS(err, "Error while reading response body", "query", query)
+				continue
 			}
-			klog.Infof("hi %s", string(body))
-			klog.Infof("Pod: %s, Container: %s, CPU: %v, Memory: %v", pod.Name, container.Name, container.Usage[k8sapiv1.ResourceCPU], container.Usage[k8sapiv1.ResourceMemory])
-			
-			var result map[string]interface{}
-			if err := json.Unmarshal(body, &result); err != nil {
-				log.Fatalf("Error unmarshalling JSON: %v", err)
+
+			var responseBody map[string]interface{}
+			if err := json.Unmarshal(body, &responseBody); err != nil {
+				klog.ErrorS(err, "Error unmarshalling JSON", "body", string(body))
+				continue
 			}
-			
-			data, ok := (result["data"]).(map[string]interface{})
+			data, ok := (responseBody["data"]).(map[string]interface{})
 			if !ok {
-				log.Fatalf("Error getting data from JSON: %v", err)
+				klog.ErrorS(err, "Error getting .data from JSON", "body", responseBody)
+				continue
 			}
-			um, ok := (data["result"]).([]interface{})
+			result, ok := (data["result"]).([]interface{})
 			if !ok {
-				log.Fatalf("Error getting result from JSON: %v", err)
+				klog.ErrorS(err, "Error getting .data.result from JSON", "body", responseBody)
+				continue
 			}
-			first, ok := (um[0]).(map[string]interface{})
+			if len(result) == 0 {
+				klog.ErrorS(err, "No query results in .data.result", "body", responseBody)
+				continue
+			}
+			if len(result) > 1 {
+				klog.ErrorS(err, "More than one query result in .data.result", "body", responseBody)
+				// Proceed for now and just use the first result.
+			}
+			first, ok := (result[0]).(map[string]interface{})
 			if !ok {
-				log.Fatalf("Error getting first from JSON: %v", err)
+				klog.ErrorS(err, "Error getting first element from .data.result", "body", responseBody)
+				continue
 			}
 			value, ok := (first["value"]).([]interface{})
 			if !ok {
-				log.Fatalf("Error getting value from JSON: %v", err)
+				klog.ErrorS(err, "Error getting .data.result[0].value", "body", responseBody)
+				continue
 			}
-			quantity, ok := (value[1]).(string)
+			rssBytes, ok := (value[1]).(string)
 			if !ok {
-				log.Fatalf("Error getting value from JSON: %v", err)
+				klog.ErrorS(err, "Error getting .data.result[0].value[1]", "body", responseBody)
+				continue
 			}
-			
 
-			resourcequantity := resource.MustParse(quantity)
-			log.Printf("Resource Quantity: %+v", resourcequantity)
+			rssBytesQuantity := resource.MustParse(rssBytes)
+			klog.InfoS("RSS (Bytes)", "container", container.Name, "pod", pod.Name, "namespace", pod.Namespace, "rssBytes", rssBytesQuantity)
+			podMetrics.Items[i].Containers[j].Usage[ResourceRSS] = &rssBytesQuantity
+			klog.InfoS("Container Usage", "container", container.Name, "pod", pod.Name, "namespace", pod.Namespace, "usage", podMetrics.Items[i].Containers[j].Usage)
 		}
 	}
 
@@ -225,107 +243,3 @@ func (s *externalMetricsClient) List(ctx context.Context, namespace string, opts
 	}
 	return &result, nil
 }
-
-// // podMetricsSource is the metrics-client source of metrics.
-// type podMetricsSource struct {
-// 	metricsGetter resourceclient.PodMetricsesGetter
-// }
-
-// // NewPodMetricsesSource Returns a Source-wrapper around PodMetricsesGetter.
-// func NewPodMetricsesSource(source resourceclient.PodMetricsesGetter) PodMetricsLister {
-// 	return podMetricsSource{metricsGetter: source}
-// }
-
-// func (s podMetricsSource) List(ctx context.Context, namespace string, opts v1.ListOptions) (*v1beta1.PodMetricsList, error) {
-// 	podMetricsInterface := s.metricsGetter.PodMetricses(namespace)
-// 	return podMetricsInterface.List(ctx, opts)
-// }
-
-// type customMetricsClient struct {
-// 	metricsClient resourceclient.PodMetricsesGetter
-// 	customMetricsClient custom_metrics.CustomMetricsClient
-// 	clusterState   *model.ClusterState
-// }
-
-// // NewAugmentedMetricsSource returns a Source for Metrics and Custom Metrics Clients.
-// func NewAugmentedMetricsSource(c *rest.Config, clusterState *model.ClusterState) PodMetricsLister {
-// 	customMetricsClient, err := custom_metrics.NewForConfig(c)
-// 	if err != nil {
-// 		klog.Fatalf("Failed initializing custom metrics client: %v", err)
-// 	}
-
-// 	return &customMetricsClient{
-// 		metricsClient: resourceclient.NewForConfigOrDie(c),
-// 		customMetricsClient: customMetricsClient,
-// 		clusterState:   clusterState,
-// 	}
-// }
-
-// func (s customMetricsClient) List(ctx context.Context, namespace string, opts v1.ListOptions) (*v1beta1.PodMetricsList, error) {
-// 	podMetricsInterface := s.metricsGetter.PodMetricses(namespace)
-// 	podMetrics := podMetricsInterface.List(ctx, opts)
-	
-// 	result := v1beta1.PodMetricsList{}
-
-// 	for _, vpa := range s.clusterState.Vpas {
-// 		if vpa.PodCount == 0 {
-// 			continue
-// 		}
-
-// 		if namespace != "" && vpa.ID.Namespace != namespace {
-// 			continue
-// 		}
-
-// 		nsClient := s.externalClient.NamespacedMetrics(vpa.ID.Namespace)
-// 		pods := s.clusterState.GetMatchingPods(vpa)
-
-// 		for _, pod := range pods {
-// 			nsClient..GetForObject(schema.GroupKind{Group: "", Kind: "Pod"}, resourceName, metricName, metav1.GetOptions{})
-// 			podNameReq, err := labels.NewRequirement("pod", selection.Equals, []string{pod.PodName})
-// 			if err != nil {
-// 				return nil, err
-// 			}
-// 			selector := vpa.PodSelector.Add(*podNameReq)
-// 			podMets := v1beta1.PodMetrics{
-// 				TypeMeta:   v1.TypeMeta{},
-// 				ObjectMeta: v1.ObjectMeta{Namespace: vpa.ID.Namespace, Name: pod.PodName},
-// 				Window:     v1.Duration{},
-// 				Containers: make([]v1beta1.ContainerMetrics, 0),
-// 			}
-// 			// Query each resource in turn, then assemble back to a single []ContainerMetrics.
-// 			containerMetrics := make(map[string]k8sapiv1.ResourceList)
-// 			for resourceName, metricName := range s.options.ResourceMetrics {
-// 				m, err := nsClient.List(metricName, selector)
-// 				if err != nil {
-// 					return nil, err
-// 				}
-// 				if m == nil || len(m.Items) == 0 {
-// 					klog.V(4).Infof("External Metrics Query for VPA %+v: resource %+v, metric %+v, No items,", vpa.ID, resourceName, metricName)
-// 					continue
-// 				}
-// 				klog.V(4).Infof("External Metrics Query for VPA %+v: resource %+v, metric %+v, %d items, item[0]: %+v", vpa.ID, resourceName, metricName, len(m.Items), m.Items[0])
-// 				podMets.Timestamp = m.Items[0].Timestamp
-// 				if m.Items[0].WindowSeconds != nil {
-// 					podMets.Window = v1.Duration{Duration: time.Duration(*m.Items[0].WindowSeconds) * time.Second}
-// 				}
-// 				for _, val := range m.Items {
-// 					ctrName, hasCtrName := val.MetricLabels[s.options.ContainerNameLabel]
-// 					if !hasCtrName {
-// 						continue
-// 					}
-// 					if containerMetrics[ctrName] == nil {
-// 						containerMetrics[ctrName] = make(k8sapiv1.ResourceList)
-// 					}
-// 					containerMetrics[ctrName][resourceName] = val.Value
-// 				}
-
-// 			}
-// 			for cname, res := range containerMetrics {
-// 				podMets.Containers = append(podMets.Containers, v1beta1.ContainerMetrics{Name: cname, Usage: res})
-// 			}
-// 			result.Items = append(result.Items, podMets)
-
-// 		}
-// 	}
-// 	return &result, nil
-// }
