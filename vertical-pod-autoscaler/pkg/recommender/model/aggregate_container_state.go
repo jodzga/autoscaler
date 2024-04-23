0b640230a7a325a1c924595265268686c0985aed
Kathy Lee <kathy.lee@databricks.com>
2024-04-22 21:36:13 -0700

    capitalization

--- vertical-pod-autoscaler/pkg/recommender/model/aggregate_container_state.go
+++ vertical-pod-autoscaler/pkg/recommender/model/aggregate_container_state.go
@@ -280,8 +280,8 @@ func (a *AggregateContainerState) SaveToCheckpoint() (*vpa_types.VerticalPodAuto
 		TotalSamplesCount: a.TotalSamplesCount,
 		MemoryHistogram:   *memory,
 		CPUHistogram:      *cpu,
-		rssBytes: a.RssBytes,
-		jVMHeapCommittedBytes: a.JVMHeapCommittedBytes,
+		RssBytes: a.RssBytes,
+		JVMHeapCommittedBytes: a.JVMHeapCommittedBytes,
 		Version:           SupportedCheckpointVersion,
 	}, nil
 }
