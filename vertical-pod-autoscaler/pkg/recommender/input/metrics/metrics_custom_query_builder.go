d4fb0d39dd58c9de1c4a76a9c417cd6c5de1cbfe
Kathy Lee <kathy.lee@databricks.com>
2024-05-22 23:05:11 -0700

    lint

--- vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_custom_query_builder.go
+++ vertical-pod-autoscaler/pkg/recommender/input/metrics/metrics_custom_query_builder.go
@@ -1,6 +1,19 @@
-// Package metrics includes custom per-container usage metrics from M3.
-// Helper for building queries for custom metrics queries.
-// See custom_metrics_lister.go for usage.
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package metrics
 
 import (
