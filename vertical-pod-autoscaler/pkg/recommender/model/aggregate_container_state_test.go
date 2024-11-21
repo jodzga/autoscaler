84ef6694b1c3dc74ead91435a2839a0929103688
leekathy <pidragon3.14@gmail.com>
2024-11-20 17:17:51 -0800

    fix unit test

--- vertical-pod-autoscaler/pkg/recommender/model/aggregate_container_state_test.go
+++ vertical-pod-autoscaler/pkg/recommender/model/aggregate_container_state_test.go
@@ -202,14 +202,13 @@ func TestAggregateContainerStateLoadFromCheckpoint(t *testing.T) {
 func TestAggregateContainerStateIsExpired(t *testing.T) {
 	cs := NewAggregateContainerState()
 	cs.LastSampleStart = testTimestamp
-	cs.TotalSamplesCount = 1
-	assert.False(t, cs.isExpired(testTimestamp.Add(90*24*time.Hour)))
+	cs.AggregateCPUUsage.AddSample(1, 33, testTimestamp)
+	assert.False(t, cs.isExpired(testTimestamp.Add(89*24*time.Hour)))
 	assert.True(t, cs.isExpired(testTimestamp.Add(91*24*time.Hour)))
 
 	csEmpty := NewAggregateContainerState()
-	csEmpty.TotalSamplesCount = 0
 	csEmpty.CreationTime = testTimestamp
-	assert.False(t, csEmpty.isExpired(testTimestamp.Add(90*24*time.Hour)))
+	assert.False(t, csEmpty.isExpired(testTimestamp.Add(89*24*time.Hour)))
 	assert.True(t, csEmpty.isExpired(testTimestamp.Add(91*24*time.Hour)))
 }
 
